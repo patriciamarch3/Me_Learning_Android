89cedd98010719abf54209894d0d3834ac6a8eef
minyi.hu <patricia.march3.2013@gmail.com>
2020-03-17 18:06:34 +0800

    prevent keyboard from automatically displaying at the beginning of an activity
    maintain the status of book detail page so that it can be restored no matter how the orientation changes

--- AndroidStudioProjects/ChallengeRoomAPI/app/src/main/java/com/example/challengeroomapi/activities/DetailActivity.java
+++ AndroidStudioProjects/ChallengeRoomAPI/app/src/main/java/com/example/challengeroomapi/activities/DetailActivity.java
@@ -1,20 +1,10 @@
 package com.example.challengeroomapi.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import com.example.challengeroomapi.R;
-import com.example.challengeroomapi.repositories.BooksViewModel;
-import com.example.challengeroomapi.room.Book;
-
-import java.util.Locale;
 
 public class DetailActivity extends AppCompatActivity {
 
